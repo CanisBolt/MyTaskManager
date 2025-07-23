33cb7d0aab04c1b2f7c8cf9666f6fd0af35584e7
CanisBolt <canisbolt@gmail.com>
2025-07-23 18:15:38 +0300

    Changed loading method. Now user must pick a save file instead of writing it manually

--- MyTaskManager/Program.cs
+++ MyTaskManager/Program.cs
@@ -19,7 +19,6 @@ do
         Console.WriteLine("Неверный ввод.");
         PrintEntryText();
     }
-
     switch (choice)
     {
         case 1:
@@ -50,6 +49,7 @@ void PrintEntryText()
         "4. Сохранение задач\n" +
         "5. Загрузка задач\n" +
         "0. Выйти из программы");
+    Console.WriteLine();
 }
 
 void PrintTasks()
@@ -97,12 +97,12 @@ void DeleteTask()
 
     PrintTasks();
 
-    Console.WriteLine("Введите порядковый номер задачи, которую хотите удалить. (Для отмены введите 0)");
+    Console.WriteLine("Введите порядковый номер задачи, которую хотите удалить. (Для отмены введите 0): ");
     int choice = -1;
     while (!int.TryParse(Console.ReadLine(), out choice) || choice < 0 || choice > AllTasks.Count)
     {
         Console.WriteLine("Неверный ввод.");
-        Console.WriteLine("Введите порядковый номер задачи, которую хотите удалить. (Для отмены введите 0)");
+        Console.WriteLine("Введите порядковый номер задачи, которую хотите удалить. (Для отмены введите 0): ");
     }
     if (choice == 0) return;
     AllTasks.RemoveAt(choice - 1);
@@ -129,7 +129,7 @@ async Task SaveTasksAsync()
     string fullSavePath = Path.Combine(SaveDirectory, saveName);
     if (File.Exists(fullSavePath))
     {
-        Console.WriteLine("Внимание, данные будут полностью перезаписаны! Вы уверены, что хотите продолжить? (да/нет)");
+        Console.WriteLine("Внимание, данные будут полностью перезаписаны! Вы уверены, что хотите продолжить? (да/нет): ");
         string answer = Console.ReadLine();
         while (!answer.ToLower().Equals("да") && !answer.ToLower().Equals("нет"))
         {
@@ -158,42 +158,57 @@ async Task SaveTasksAsync()
 
 async Task LoadTasks()
 {
-    Console.WriteLine("Введите имя файла сохранения. Чувствителен к регистру!"); //TODO change to select file
-    string saveName = Console.ReadLine();
-
-    while (saveName.Length < 3)
+    Console.WriteLine();
+    string dirName = "Saves";
+    if (Directory.Exists(dirName))
     {
-        Console.WriteLine("Неверный ввод. Введите имя не менее трех символов!");
-        saveName = Console.ReadLine();
-    }
-    saveName += ".json";
-    string fullSavePath = Path.Combine(SaveDirectory, saveName);
-
-    if (!File.Exists(fullSavePath))
-    {
-        Console.WriteLine("Файла с таким именем не существует!");
-        return;
-    }
-
-    if(AllTasks.Count > 0)
-    {
-        Console.WriteLine("Внимание! Текущие данные будут стерты! Вы уверены, что хотите продолжить? (да/нет)");
-        string answer = Console.ReadLine();
-        while (!answer.ToLower().Equals("да") && !answer.ToLower().Equals("нет"))
+        string[] files = Directory.GetFiles(dirName, "*.json");
+        if(files.Length == 0)
         {
-            Console.WriteLine("Неверный ввод. Введите да или нет.");
-            answer = Console.ReadLine();
-        }
-        if (answer.ToLower().Equals("нет"))
-        {
-            Console.WriteLine("Операция загрузки отменена!");
+            Console.WriteLine("Нет файлов для загрузки:");
             return;
         }
+
+        Console.WriteLine("Список файлов для загрузки:");
+        for (int i = 0; i < files.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {files[i]}");
+        }
+
+        int choice = -1;
+        Console.WriteLine("Введите порядковый номер файла для загрузки. 0 для отмены загрузки: ");
+        while (!int.TryParse(Console.ReadLine(), out choice) || choice < 0 || choice > files.Length)
+        {
+            Console.WriteLine("Неверный ввод.");
+        }
+        if (choice == 0)
+        {
+            return;
+        }
+
+        string saveName = files[choice-1].ToString();
+
+        AllTasks = new List<UserTask>();
+        string json = await File.ReadAllTextAsync(saveName);
+
+        if (AllTasks.Count > 0)
+        {
+            Console.WriteLine("Внимание! Текущие данные будут стерты! Вы уверены, что хотите продолжить? (да/нет)");
+            string answer = Console.ReadLine();
+            while (!answer.ToLower().Equals("да") && !answer.ToLower().Equals("нет"))
+            {
+                Console.WriteLine("Неверный ввод. Введите да или нет.");
+                answer = Console.ReadLine();
+            }
+            if (answer.ToLower().Equals("нет"))
+            {
+                Console.WriteLine("Операция загрузки отменена!");
+                return;
+            }
+        }
+
+        AllTasks = JsonSerializer.Deserialize<List<UserTask>>(json);
+        Console.WriteLine("Данные успешно загружены!");
     }
-
-    AllTasks = new List<UserTask>();
-    string json = await File.ReadAllTextAsync(fullSavePath);
-
-    AllTasks = JsonSerializer.Deserialize<List<UserTask>>(json);
-    Console.WriteLine("Данные успешно загружены!");
+    Console.WriteLine();
 }
