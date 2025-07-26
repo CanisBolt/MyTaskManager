6e081aa63a1ded2c27d32649b955811c6b07780b
CanisBolt <canisbolt@gmail.com>
2025-07-26 18:46:20 +0300

    Some code clean ups (extracted user input in separate methods). Added ability to delete archived tasks.

--- MyTaskManager/Program.cs
+++ MyTaskManager/Program.cs
@@ -5,22 +5,16 @@ using System.Xml.Linq;
 const string SaveDirectory = "Saves";
 TaskManagerData taskManagerData = new TaskManagerData();
 
-int choice = -1;
-
 if (!Directory.Exists(SaveDirectory))
 {
     Directory.CreateDirectory(SaveDirectory);
 }
+int mainChoice = -1;
 
 do
 {
-    PrintEntryText();
-    while(!int.TryParse(Console.ReadLine(), out choice) || choice < 0 || choice > 7)
-    {
-        Console.WriteLine("Неверный ввод.");
-        PrintEntryText();
-    }
-    switch (choice)
+    mainChoice = GetUserInput(0, 7, PrintEntryText());
+    switch (mainChoice)
     {
         case 1:
             CheckTasks();
@@ -44,12 +38,12 @@ do
             MarkAsCompleted();
             break;
     }
-} while(choice != 0);
+} while(mainChoice != 0);
 
 
-void PrintEntryText()
+string PrintEntryText()
 {
-    Console.WriteLine("Выберите действие:\n" +
+    return "Выберите действие:\n" +
         "1. Просмотр задач\n" +
         "2. Добавление задач\n" +
         "3. Удаление задач\n" +
@@ -57,12 +51,16 @@ void PrintEntryText()
         "5. Загрузка задач\n" +
         "6. Редактировать задачу\n" +
         "7. Отметить как выполненную\n" +
-        "0. Выйти из программы");
-    Console.WriteLine();
+        "0. Выйти из программы";
 }
 
-void PrintTasks()
+bool PrintActiveTasks()
 {
+    if(taskManagerData.AllTasks.Count == 0)
+    {
+        Console.WriteLine("Нет активных задач. Сначала необходимо добавить хотя бы одну задачу.");
+        return true;
+    }
     int taskCount = 1;
     foreach (UserTask task in taskManagerData.AllTasks)
     {
@@ -73,13 +71,18 @@ void PrintTasks()
         taskCount++;
         Console.WriteLine();
     }
-    Console.WriteLine("-------------------------------------------------\n" +
-        "\tАРХИВ:\n");
-    PrintArchiveTasks();
+    return false;
 }
 
-void PrintArchiveTasks()
+bool PrintArchiveTasks()
 {
+    if (taskManagerData.ArchiveTasks.Count == 0)
+    {
+        Console.WriteLine("Нет архивных задач. Сначала необходимо добавить хотя бы одну задачу.");
+        return true;
+    }
+    Console.WriteLine("-------------------------------------------------\n" +
+        "\tАРХИВ:\n");
     int taskCount = 1;
     foreach (UserTask task in taskManagerData.ArchiveTasks)
     {
@@ -90,17 +93,19 @@ void PrintArchiveTasks()
         taskCount++;
         Console.WriteLine();
     }
+    return false;
 }
 
 bool CheckTasks()
 {
     Console.Clear();
-    if (taskManagerData.AllTasks.Count == 0)
+    if(taskManagerData.AllTasks.Count == 0 && taskManagerData.ArchiveTasks.Count == 0)
     {
         Console.WriteLine("Нет задач. Сначала необходимо добавить хотя бы одну задачу.");
         return true;
     }
-    PrintTasks();
+    PrintActiveTasks();
+    PrintArchiveTasks();
     return false;
 }
 
@@ -119,21 +124,34 @@ void AddTask()
 
 void DeleteTask()
 {
-    if(CheckTasks())
+    if (CheckTasks())
     {
         return;
     }
-
-    Console.WriteLine("Введите порядковый номер задачи, которую хотите удалить. (Для отмены введите 0): ");
-    int choice = -1;
-    while (!int.TryParse(Console.ReadLine(), out choice) || choice < 0 || choice > taskManagerData.AllTasks.Count)
+    int choice = GetUserInput(1, 2, "Введите 1 для удаления активных задач, 2 для удаления архивных задач: ");
+    switch(choice)
     {
-        Console.WriteLine("Неверный ввод.");
-        Console.WriteLine("Введите порядковый номер задачи, которую хотите удалить. (Для отмены введите 0): ");
+        case 1:
+            if(PrintActiveTasks())
+            {
+                DeleteTasks(taskManagerData.AllTasks);
+            }
+            break;
+        case 2:
+            if (PrintArchiveTasks())
+            {
+                DeleteTasks(taskManagerData.ArchiveTasks);
+            }
+            break;
     }
+}
+
+void DeleteTasks(List<UserTask> taskList)
+{
+    int choice = GetUserInput(0, taskList.Count, "Введите порядковый номер задачи, которую хотите удалить. (Для отмены введите 0): ");
     if (choice == 0) return;
     choice--;
-    taskManagerData.AllTasks.RemoveAt(choice);
+    taskList.RemoveAt(choice);
     Console.Clear();
     Console.WriteLine("Задача успешно удалена!");
     Console.WriteLine();
@@ -145,51 +163,40 @@ void EditTask()
     {
         return;
     }
-
-    Console.WriteLine("Выберите задачу для редактирования: ");
-    int choice = -1;
-    Console.WriteLine("Введите порядковый номер файла для редактирования. 0 для отмены редактирования: ");
-    while (!int.TryParse(Console.ReadLine(), out choice) || choice < 0 || choice > taskManagerData.AllTasks.Count)
-    {
-        Console.WriteLine("Неверный ввод.");
-    }
+    int choice = GetUserInput(0, taskManagerData.AllTasks.Count, "Введите порядковый номер файла для редактирования. 0 для отмены редактирования: ");
     if (choice == 0)
     {
         return;
     }
-    UserTask editTask = new UserTask(taskManagerData.AllTasks[choice - 1].Name, taskManagerData.AllTasks[choice - 1].Description, taskManagerData.AllTasks[choice - 1].Created, taskManagerData.AllTasks[choice - 1].TaskPriority);
     bool isFileChanged = false;
 
     string name = GetTaskNameInput();
 
     if (!string.IsNullOrEmpty(name))
     {
-        editTask.Name = name;
+        taskManagerData.AllTasks[choice].Name = name;
         isFileChanged = true;
     }
 
     string description = GetTaskDescriptionInput();
     if (!string.IsNullOrEmpty(description))
     {
-        editTask.Description = description;
+        taskManagerData.AllTasks[choice].Description = description;
         isFileChanged = true;
     }
 
 
     int taskChoice = GetTaskPriorityInput() - 1;
-    if (!editTask.TaskPriority.Equals(UserTask.SetTaskPriority(taskChoice)))
+    if (!taskManagerData.AllTasks[choice].TaskPriority.Equals(UserTask.SetTaskPriority(taskChoice)))
     {
-        editTask.TaskPriority = UserTask.SetTaskPriority(taskChoice);
-        isFileChanged |= true;
+        taskManagerData.AllTasks[choice].TaskPriority = UserTask.SetTaskPriority(taskChoice);
+        isFileChanged = true;
     }
 
     choice--;
     if (isFileChanged)
     {
-        taskManagerData.AllTasks[choice].Name = editTask.Name;
-        taskManagerData.AllTasks[choice].Description = editTask.Description;
         taskManagerData.AllTasks[choice].Created = DateTime.Now;
-        taskManagerData.AllTasks[choice].TaskPriority = editTask.TaskPriority;
     }
     Console.Clear();
     Console.WriteLine("Задача успешно обновлена!");
@@ -201,14 +208,8 @@ void MarkAsCompleted()
     {
         return;
     }
-
-    Console.WriteLine("Выберите задачу: ");
-    int choice = -1;
-    Console.WriteLine("Введите порядковый номер файла для редактирования. 0 для отмены редактирования: ");
-    while (!int.TryParse(Console.ReadLine(), out choice) || choice < 0 || choice > taskManagerData.AllTasks.Count)
-    {
-        Console.WriteLine("Неверный ввод.");
-    }
+    PrintActiveTasks();
+    int choice = GetUserInput(0, taskManagerData.AllTasks.Count, "Введите порядковый номер файла для редактирования. 0 для отмены редактирования: ");
     if (choice == 0)
     {
         return;
@@ -216,22 +217,12 @@ void MarkAsCompleted()
 
     choice--;
     Console.WriteLine("Отметить задачу как выполненную?(да/нет)");
-    string answer = Console.ReadLine();
-    while (!answer.ToLower().Equals("да") && !answer.ToLower().Equals("нет"))
-    {
-        Console.WriteLine("Неверный ввод. Введите да или нет.");
-        answer = Console.ReadLine();
-    }
+    string answer = GetUserInputYesNo();
     if (answer.ToLower().Equals("да"))
     {
         taskManagerData.AllTasks[choice].IsCompleted = true;
         Console.WriteLine("Желаете переместить данную задачу в архив?(да/нет)");
-        answer = Console.ReadLine();
-        while (!answer.ToLower().Equals("да") && !answer.ToLower().Equals("нет"))
-        {
-            Console.WriteLine("Неверный ввод. Введите да или нет.");
-            answer = Console.ReadLine();
-        }
+        answer = GetUserInputYesNo();
         if (answer.ToLower().Equals("да"))
         {
             taskManagerData.ArchiveTasks.Add(new UserTask(taskManagerData.AllTasks[choice].Name, taskManagerData.AllTasks[choice].Description, taskManagerData.AllTasks[choice].Created, DateTime.Now));
@@ -241,12 +232,7 @@ void MarkAsCompleted()
         else
         {
             Console.WriteLine("Желаете удалить данную задачу из списка?(да/нет)");
-            answer = Console.ReadLine();
-            while (!answer.ToLower().Equals("да") && !answer.ToLower().Equals("нет"))
-            {
-                Console.WriteLine("Неверный ввод. Введите да или нет.");
-                answer = Console.ReadLine();
-            }
+            answer = GetUserInputYesNo();
             if (answer.ToLower().Equals("да"))
             {
                 taskManagerData.AllTasks.Remove(taskManagerData.AllTasks[choice]);
@@ -278,16 +264,10 @@ async Task SaveTasksAsync()
     if (File.Exists(fullSavePath))
     {
         Console.WriteLine("Внимание, данные будут полностью перезаписаны! Вы уверены, что хотите продолжить? (да/нет): ");
-        string answer = Console.ReadLine();
-        while (!answer.ToLower().Equals("да") && !answer.ToLower().Equals("нет"))
-        {
-            Console.WriteLine("Неверный ввод. Введите да или нет.");
-            answer = Console.ReadLine();
-        }
+        string answer = GetUserInputYesNo();
         if (answer.ToLower().Equals("да"))
         {
             await Saving(fullSavePath);
-            Console.WriteLine("Задачи успешно сохранены!");
         }
         else if (answer.ToLower().Equals("нет"))
         {
@@ -298,10 +278,10 @@ async Task SaveTasksAsync()
     else
     {
         await Saving(fullSavePath);
-        Console.Clear();
-        Console.WriteLine("Задачи успешно сохранены!");
-        Console.WriteLine();
     }
+    Console.Clear();
+    Console.WriteLine("Задачи успешно сохранены!");
+    Console.WriteLine();
 }
 
 async Task LoadTasksAsync()
@@ -323,12 +303,7 @@ async Task LoadTasksAsync()
             Console.WriteLine($"{i + 1}. {Path.GetFileName(files[i])}");
         }
 
-        int choice = -1;
-        Console.WriteLine("Введите порядковый номер файла для загрузки. 0 для отмены загрузки: ");
-        while (!int.TryParse(Console.ReadLine(), out choice) || choice < 0 || choice > files.Length)
-        {
-            Console.WriteLine("Неверный ввод.");
-        }
+        int choice = GetUserInput(0, files.Length, "Введите порядковый номер файла для загрузки. 0 для отмены загрузки: ");
         if (choice == 0)
         {
             return;
@@ -339,12 +314,7 @@ async Task LoadTasksAsync()
         if (taskManagerData.AllTasks.Count > 0 || taskManagerData.ArchiveTasks.Count > 0)
         {
             Console.WriteLine("Внимание! Текущие данные будут стерты! Вы уверены, что хотите продолжить? (да/нет)");
-            string answer = Console.ReadLine();
-            while (!answer.ToLower().Equals("да") && !answer.ToLower().Equals("нет"))
-            {
-                Console.WriteLine("Неверный ввод. Введите да или нет.");
-                answer = Console.ReadLine();
-            }
+            string answer = GetUserInputYesNo();
             if (answer.ToLower().Equals("нет"))
             {
                 Console.WriteLine("Операция загрузки отменена!");
@@ -434,21 +404,31 @@ static string GetTaskDescriptionInput()
 
 static int GetTaskPriorityInput()
 {
-    Console.WriteLine("Выберите степень важности задачи:\n" +
-            "1. Низкая\n" +
-            "2. Средняя\n" +
-            "3. Высокая\n" +
-            "4. Срочная");
+    int choice = GetUserInput(1, 4, "Выберите степень важности задачи:\\n\" +\r\n        \"1. Низкая\\n\" +\r\n        \"2. Средняя\\n\" +\r\n        \"3. Высокая\\n\" +\r\n        \"4. Срочная\"");
+    return choice;
+}
+
+static int GetUserInput(int minValue, int maxValue, string message)
+{
+    Console.WriteLine(message);
     int choice = -1;
-    while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 4)
+    while (!int.TryParse(Console.ReadLine(), out choice) || choice <  minValue || choice > maxValue)
     {
         Console.WriteLine("Неверный ввод.");
-        Console.WriteLine("Выберите степень важности задачи:\n" +
-        "1. Низкая\n" +
-        "2. Средняя\n" +
-        "3. Высокая\n" +
-        "4. Срочная");
+        Console.WriteLine(message);
     }
 
     return choice;
+}
+
+static string GetUserInputYesNo()
+{
+    string answer = Console.ReadLine();
+    while (!answer.ToLower().Equals("да") && !answer.ToLower().Equals("нет"))
+    {
+        Console.WriteLine("Неверный ввод. Введите да или нет.");
+        answer = Console.ReadLine();
+    }
+
+    return answer;
 }
